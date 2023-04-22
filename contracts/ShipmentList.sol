d935b763c0976137ab61a061361de50c8cb20014
zero0ne11 <gautam_11912008@nitkkr.ac.in>
2023-04-22 16:04:55 +0530

    update shipment list and user list migrations

--- contracts/ShipmentList.sol
+++ contracts/ShipmentList.sol
@@ -2,9 +2,11 @@
 pragma solidity <=0.8.19;
 
 contract ShipmentList{
-    uint public numberOfShipments = 0;
-    mapping(uint => Shipment) public listOfShipments;
+    mapping(uint=>uint) public numberOfShipments;
+    uint public numberOfChains = 0;
+    mapping(uint => mapping(uint=>Shipment)) public listOfShipments;
     struct Shipment{
+        uint chainId;
         uint shipmentId;
         uint medicineId;
         address senderId;
@@ -13,41 +15,54 @@ contract ShipmentList{
         string deliveryStatus;
         bool transactionStatus;
     }
-    function createShipment(uint medicineId, address recieverId, string memory deliveryStatus) public{
+    function createShipment(bool newShipment, uint chainId, uint medicineId, address recieverId, string memory deliveryStatus) public{
         // mapping(uint=> address) senderIds;
         // mapping(uint=> uint256) sendingTimes = {};
-        numberOfShipments = numberOfShipments + 1;
         address senderId = msg.sender;
         uint256 sendingTime = block.timestamp;
-        listOfShipments[numberOfShipments] = Shipment(numberOfShipments, medicineId, senderId, recieverId, sendingTime, deliveryStatus, false);
+        if(newShipment==true){
+            numberOfChains = numberOfChains+1;
+            numberOfShipments[numberOfChains]++;
+            listOfShipments[numberOfChains][numberOfShipments[numberOfChains]] = Shipment(numberOfChains, numberOfShipments[numberOfChains], medicineId, senderId, recieverId, sendingTime, deliveryStatus, false);
+        }else{
+            numberOfShipments[chainId]++;
+            listOfShipments[chainId][numberOfShipments[chainId]] = Shipment(chainId, numberOfShipments[chainId], medicineId, senderId, recieverId, sendingTime, deliveryStatus, false);
+        }
     }
 
-    function deleteShipment(uint shipmentId) public{
-        delete listOfShipments[shipmentId];
+    function deleteShipment(uint chainId, uint shipmentId) public{
+        delete listOfShipments[chainId][shipmentId];
     }
-    function updateDeliveryStatus(uint shipmentId, string memory deliveryStatus) public {
-        listOfShipments[shipmentId].deliveryStatus = deliveryStatus;
+
+    function deleteChain(uint chainId) public{
+        for(uint i = 0;i < numberOfShipments[chainId];i++){
+            delete listOfShipments[chainId][i+1];
+        }
     }
-    function setTransactionComplete(uint shipmentId) public{
-        listOfShipments[shipmentId].transactionStatus = true;
+
+    function updateDeliveryStatus(uint chainId, uint shipmentId, string memory deliveryStatus) public {
+        listOfShipments[chainId][shipmentId].deliveryStatus = deliveryStatus;
     }
-    function verifyShipment(uint shipmentId) public view returns(bool){
-        Shipment memory shipment = listOfShipments[shipmentId];
-        if(shipment.recieverId==address(0)){
-            if(shipment.transactionStatus==true) return false;
-            else{
-                shipment.transactionStatus = true;
-                return true;
-            }
+    function setTransactionComplete(uint chainId, uint shipmentId) public{
+        listOfShipments[chainId][shipmentId].transactionStatus = true;
+    }
+    function setVerified(uint chainId, uint shipmentId) public{
+        listOfShipments[chainId][shipmentId].transactionStatus = true;
+    }
+    function getVerificationResult(uint chainId, uint shipmentId) public view returns(bool){
+        if(listOfShipments[chainId][shipmentId].transactionStatus==true){
+            return false;
         }else{
-            if(shipment.recieverId==msg.sender){
-                if(shipment.transactionStatus==true) return false;
-                else{
-                    shipment.transactionStatus = true;
-                    return true;
-                }
+            if(listOfShipments[chainId][shipmentId].recieverId==msg.sender || listOfShipments[chainId][shipmentId].recieverId==address(0)){
+                return true;
+            }else{
+                return false;
             }
         }
+    }
+    function allowOpenSelling(uint chainId, uint shipmentId) public {
+        listOfShipments[chainId][shipmentId].transactionStatus = false;
+        listOfShipments[chainId][shipmentId].recieverId = address(0);
     }
 
 }
